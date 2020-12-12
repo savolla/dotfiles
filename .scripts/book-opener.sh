2aa57c5f26d0bde5533be51b4b56426d03341da1
savolla <savolla@protonmail.com>
2020-12-12 17:43:09 +0300

    Whee, good night.

--- .scripts/book-opener.sh
+++ .scripts/book-opener.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 READER="zathura"
-$READER "$(find ~/lib/* -type f -name '*.pdf' | awk -F "/" '{ print $NF }' | rofi -dmenu -i -p "Select Book: " | xargs find ~/lib/* -iname )"
+$READER "$(find ~/lib/* -type f -name '*.pdf' -o -name '*.epub' \
+ | awk -F "/" '{ print $NF }' \
+ | rofi -dmenu -i -p "Select Book: " \
+ | xargs -d'\n' find ~/lib/* -iname )"
