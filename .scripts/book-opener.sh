a2ea0642ed8483da7095d14914b90ab3ad650445
savolla <savolla@protonmail.com>
2020-12-12 16:42:33 +0300

    asdfasdfasdfasdfasdfasdfadsf

--- .scripts/book-opener.sh
+++ .scripts/book-opener.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-okular "$(find ~/lib/* -type f -name '*.pdf' | awk -F "/" '{ print $NF }' | rofi -dmenu -i -p "Select Book: " | xargs find ~/lib/* -iname )"
+READER="zathura"
+$READER "$(find ~/lib/* -type f -name '*.pdf' | awk -F "/" '{ print $NF }' | rofi -dmenu -i -p "Select Book: " | xargs find ~/lib/* -iname )"
