eddadfac5940c8f338c58ed968a92e0e4d511416
savolla <savolla@protonmail.com>
2021-01-06 15:37:16 +0300

    perfect...

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -126,8 +126,12 @@
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 17))
 
 ;; FONT OPTION 2
-(setq doom-font (font-spec :family "Fira Code" :size 17)
-      doom-variable-pitch-font (font-spec :family "Fira Code" :size 17))
+;; (setq doom-font (font-spec :family "Fira Code" :size 17)
+;;       doom-variable-pitch-font (font-spec :family "Fira Code" :size 17))
+;;
+;; FONT OPTION 4
+(setq doom-font (font-spec :family "Input" :size 20)
+      doom-variable-pitch-font (font-spec :family "Input" :size 20))
 
 ;;#######################################################################################
 
