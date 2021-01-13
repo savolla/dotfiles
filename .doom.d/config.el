e81210deec6453980e635549a5cb41a5add898b2
savolla <savolla@protonmail.com>
2021-01-13 19:01:07 +0300

    fixed some minor stuff, might need some additional work.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,10 +1,16 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
+
 ;; Custom Functions
+(defun open-notitia()
+  "Open Personal Personal Wiki file"
+  (interactive)
+  (find-file "~/txt/notitia/index.org"))
+
 (defun open-todo()
   "Open Personal TODO file"
-
-  )
+  (interactive)
+  (find-file "~/txt/todo.org"))
 
 ;; Packages ####################################################################
 '(require ob-async)
@@ -66,7 +72,7 @@
 (map! :leader :desc "toggle tagbar"           "- c t" #'lsp-ui-imenu ) ;; toggle tagbar
 (map! :leader :desc "toggle undo tree"        "- c u" #'undo-tree-visualize ) ;; toggle undo tree
 (map! :leader :desc "run gdb"                 "- c g" #'gdb ) ;; run gdb
-(map! :leader :desc "unfuck json" "- c j      " #'json-pretty-print-buffer ) ;; ranger
+(map! :leader :desc "unfuck json"             "- c j" #'json-pretty-print-buffer ) ;; ranger
 
 ;; tabs 't'
 (map! :leader :desc "tabs"                    "- t")
@@ -97,6 +103,11 @@
 
 ;; general
 (map! :leader :desc "ranger"                  "o - " #'ranger ) ;; ranger
+
+;; files
+(map! :leader :desc "notitia"                 "- n w" #'open-notitia ) ;; open personal wiki
+(map! :leader :desc "todo"                    "- n t" #'open-todo ) ;; open TODO file
+
 ;;#######################################################################################
 
 
@@ -411,223 +422,12 @@
  ;; If there is more than one, they won't work right.
  )
 
-(defun jsravn--org-inline-css-hook (exporter)
-  "Insert custom inline css to automatically set the
-   background of code to whatever theme I'm using's background"
-  (when (eq exporter 'html)
-    (setq
-     org-html-head-extra
-     (concat
-      (if (s-contains-p "<!––tec/custom-head-start-->" org-html-head-extra)
-          (s-replace-regexp "<!––tec/custom-head-start-->.*<!––tec/custom-head-end-->" "" org-html-head-extra)
-        org-html-head-extra)
-      (format "<!––tec/custom-head-start-->
-<style type=\"text/css\">
-   :root {
-      --theme-bg: %s;
-      --theme-bg-alt: %s;
-      --theme-base0: %s;
-      --theme-base1: %s;
-      --theme-base2: %s;
-      --theme-base3: %s;
-      --theme-base4: %s;
-      --theme-base5: %s;
-      --theme-base6: %s;
-      --theme-base7: %s;
-      --theme-base8: %s;
-      --theme-fg: %s;
-      --theme-fg-alt: %s;
-      --theme-grey: %s;
-      --theme-red: %s;
-      --theme-orange: %s;
-      --theme-green: %s;
-      --theme-teal: %s;
-      --theme-yellow: %s;
-      --theme-blue: %s;
-      --theme-dark-blue: %s;
-      --theme-magenta: %s;
-      --theme-violet: %s;
-      --theme-cyan: %s;
-      --theme-dark-cyan: %s;
-   }
-</style>"
-              (doom-color 'bg)
-              (doom-color 'bg-alt)
-              (doom-color 'base0)
-              (doom-color 'base1)
-              (doom-color 'base2)
-              (doom-color 'base3)
-              (doom-color 'base4)
-              (doom-color 'base5)
-              (doom-color 'base6)
-              (doom-color 'base7)
-              (doom-color 'base8)
-              (doom-color 'fg)
-              (doom-color 'fg-alt)
-              (doom-color 'grey)
-              (doom-color 'red)
-              (doom-color 'orange)
-              (doom-color 'green)
-              (doom-color 'teal)
-              (doom-color 'yellow)
-              (doom-color 'blue)
-              (doom-color 'dark-blue)
-              (doom-color 'magenta)
-              (doom-color 'violet)
-              (doom-color 'cyan)
-              (doom-color 'dark-cyan))
-      "
-<link rel='stylesheet' type='text/css' href='https://fniessen.github.io/org-html-themes/styles/readtheorg/css/htmlize.css'/>
-<link rel='stylesheet' type='text/css' href='https://fniessen.github.io/org-html-themes/styles/readtheorg/css/readtheorg.css'/>
-<script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>
-<script type='text/javascript' src='https://fniessen.github.io/org-html-themes/styles/lib/js/jquery.stickytableheaders.min.js'></script>
-<script type='text/javascript' src='https://fniessen.github.io/org-html-themes/styles/readtheorg/js/readtheorg.js'></script>
-<style>
-   pre.src {
-     background-color: var(--theme-bg);
-     color: var(--theme-fg);
-     scrollbar-color:#bbb6#9992;
-     scrollbar-width: thin;
-     margin: 0;
-     border: none;
-   }
-   div.org-src-container {
-     border-radius: 12px;
-     overflow: hidden;
-     margin-bottom: 24px;
-     margin-top: 1px;
-     border: 1px solid#e1e4e5;
-   }
-   pre.src::before {
-     background-color:#6666;
-     top: 8px;
-     border: none;
-     border-radius: 5px;
-     line-height: 1;
-     border: 2px solid var(--theme-bg);
-     opacity: 0;
-     transition: opacity 200ms;
-   }
-   pre.src:hover::before { opacity: 1; }
-   pre.src:active::before { opacity: 0; }
-   pre.example {
-     border-radius: 12px;
-     background: var(--theme-bg-alt);
-     color: var(--theme-fg);
-   }
-   code {
-     border-radius: 5px;
-     background:#e8e8e8;
-     font-size: 80%;
-   }
-   kbd {
-     display: inline-block;
-     padding: 3px 5px;
-     font: 80% SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace;
-     line-height: normal;
-     line-height: 10px;
-     color:#444d56;
-     vertical-align: middle;
-     background-color:#fafbfc;
-     border: 1px solid#d1d5da;
-     border-radius: 3px;
-     box-shadow: inset 0 -1px 0#d1d5da;
-   }
-   table {
-     max-width: 100%;
-     overflow-x: auto;
-     display: block;
-     border-top: none;
-   }
-   a {
-       text-decoration: none;
-       background-image: linear-gradient(#d8dce9, #d8dce9);
-       background-position: 0% 100%;
-       background-repeat: no-repeat;
-       background-size: 0% 2px;
-       transition: background-size .3s;
-   }
-   \#table-of-contents a {
-       background-image: none;
-   }
-   a:hover, a:focus {
-       background-size: 100% 2px;
-   }
-   a[href^='#'] { font-variant-numeric: oldstyle-nums; }
-   a[href^='#']:visited { color:#3091d1; }
-   li .checkbox {
-       display: inline-block;
-       width: 0.9em;
-       height: 0.9em;
-       border-radius: 3px;
-       margin: 3px;
-       top: 4px;
-       position: relative;
-   }
-   li.on > .checkbox { background: var(--theme-green); box-shadow: 0 0 2px var(--theme-green); }
-   li.trans > .checkbox { background: var(--theme-orange); box-shadow: 0 0 2px var(--theme-orange); }
-   li.off > .checkbox { background: var(--theme-red); box-shadow: 0 0 2px var(--theme-red); }
-   li.on > .checkbox::after {
-     content: '';
-     height: 0.45em;
-     width: 0.225em;
-     -webkit-transform-origin: left top;
-     transform-origin: left top;
-     transform: scaleX(-1) rotate(135deg);
-     border-right: 2.8px solid#fff;
-     border-top: 2.8px solid#fff;
-     opacity: 0.9;
-     left: 0.10em;
-     top: 0.45em;
-     position: absolute;
-   }
-   li.trans > .checkbox::after {
-       content: '';
-       font-weight: bold;
-       font-size: 1.6em;
-       position: absolute;
-       top: 0.23em;
-       left: 0.09em;
-       width: 0.35em;
-       height: 0.12em;
-       background:#fff;
-       opacity: 0.9;
-       border-radius: 0.1em;
-   }
-   li.off > .checkbox::after {
-    content: '✖';
-    color:#fff;
-    opacity: 0.9;
-    position: relative;
-    top: -0.40rem;
-    left: 0.17em;
-    font-size: 0.75em;
-  }
-   span.timestamp {
-       color: #003280;
-       background: #647CFF44;
-       border-radius: 3px;
-       line-height: 1.25;
-   }
-   \#table-of-contents { overflow-y: auto; }
-   blockquote p { margin: 8px 0px 16px 0px; }
-   \#postamble .date { color: var(--theme-green); }
-   ::-webkit-scrollbar { width: 10px; height: 8px; }
-   ::-webkit-scrollbar-track { background:#9992; }
-   ::-webkit-scrollbar-thumb { background:#ccc; border-radius: 10px; }
-   ::-webkit-scrollbar-thumb:hover { background:#888; }
-</style>
-<!––tec/custom-head-end-->
-"
-      ))))
 
-(add-hook 'org-export-before-processing-hook 'jsravn--org-inline-css-hook)
-
-(setq org-html-text-markup-alist
-      '((bold . "<b>%s</b>")
-        (code . "<code>%s</code>")
-        (italic . "<i>%s</i>")
-        (strike-through . "<del>%s</del>")
-        (underline . "<span class=\"underline\">%s</span>")
-        (verbatim . "<kbd>%s</kbd>")))
+;; Startup
+;; ---------------------------
+(+workspace/rename "Main" )
+(+workspace/new "2nd Brain" )
+(find-file "~/txt/notitia/index.org")
+(evil-window-vsplit)
+(find-file "~/txt/todo.org")
+(+workspace/switch-to-0)
