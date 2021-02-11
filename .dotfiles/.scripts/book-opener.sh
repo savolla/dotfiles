fd899fa91ec0e0c502faa35dabcf1878e22b8784
savolla <savolla@protonmail.com>
2021-02-11 04:59:56 +0300

    DNS_PROBE_FINISHED_NXDOMAIN

--- .dotfiles/.scripts/book-opener.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-READER="zathura"
-LIB_PATH="/mnt/c/Users/savolla/Desktop/savolla/lib"
-$READER "$(find $LIB_PATH/* -type f -name '*.pdf' -o -name '*.epub' \
- | awk -F "/" '{ print $NF }' \
- | rofi -dmenu -i -p "Select Book: " \
- | xargs -d'\n' find $LIB_PATH/* -iname )"
