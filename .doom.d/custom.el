632fa32666dd7b6537aecce06ff194d8fa2dd30b
savolla <savolla@protonmail.com>
2021-04-06 01:06:12 +0300

    This branch is so dirty, even your mom can't clean it.

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -18,7 +18,7 @@
  '(jdee-db-spec-breakpoint-face-colors (cons "#1B2229" "#3f444a"))
  '(objed-cursor-color "#ff6c6b")
  '(package-selected-packages
-   '(dirtree platformio-mode elfeed-web snow meghanada lsp-java exwm emms))
+   '(nov dirtree platformio-mode elfeed-web snow meghanada lsp-java exwm emms))
  '(pdf-view-midnight-colors (cons "#bbc2cf" "#282c34"))
  '(rustic-ansi-faces
    ["#282c34" "#ff6c6b" "#98be65" "#ECBE7B" "#51afef" "#c678dd" "#46D9FF" "#bbc2cf"])
@@ -49,6 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(link ((t (:weight light))))
  '(outline-1 ((t (:weight extra-bold :height 1.12))))
  '(outline-2 ((t (:weight bold :height 1.1))))
  '(outline-3 ((t (:weight bold :height 1.08))))
