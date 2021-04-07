fc86261dcfcfff55348a1b0f6f4ca8458d7832a4
savolla <savolla@protonmail.com>
2021-04-07 23:49:27 +0300

    fixed the israeli-palestinian conflict

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -33,7 +33,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       (ligatures +extra +pragmata-pro)
+       ;; (ligatures +extra +fira)
        ;; minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
