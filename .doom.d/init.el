b6258fbc26a2904ed9058c86ae80f87592777f2e
Oleksiy Nehlyadyuk <savolla@protonmail.com>
2021-05-23 09:10:12 +0300

    Herp derp I left the debug in there and forgot to reset errors.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -131,10 +131,10 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       ;;(haskell +dante)  ; a language that's lazier than I am
+       (haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ; a language you can depend on
-       ;;json              ; At least it ain't XML
+       json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
