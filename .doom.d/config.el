5e6cf6b22996947d90954e437cc213e1aa2d89a0
Oleksiy Nehlyadyuk <savolla@protonmail.com>
2021-05-30 17:31:27 +0300

    David rebase plx?

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -223,7 +223,7 @@
         (
          "p" "person" plain #'org-roam-capture--get-point
          :file-name "person/${title}"
-         :head "#+TITLE: ${title}\n#+STARTUP: overview\n#+ROAM_TAGS: person\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n- reference ::\n- phone ::\n- mail ::\n- lives ::\n\n* who is it?\n%?\n"
+         :head "#+TITLE: ${title}\n#+STARTUP: overview\n#+ROAM_TAGS: person\n#+CREATED: %u\n#+LAST_MODIFIED: %U\n- reference ::\n- phone ::\n- mail ::\n- website ::\n- lives ::\n\n* who is ${title}?\n%?\n"
          :unnarrowed t
         )
         (
