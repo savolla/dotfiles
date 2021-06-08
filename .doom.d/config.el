ed03bf9fb5ea767ed39479c4942f0a87efe88e9a
Oleksiy Nehlyadyuk <savolla@protonmail.com>
2021-06-08 21:55:23 +0300

    This should work until december 2013.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -61,11 +61,11 @@
     ;; :vslot -4
     :select t
     :quit nil))
-(setq org-roam-directory "~/txt/zattel"
-      ;; org-roam-directory "~/txt/roam"
+(setq org-roam-directory "~/txt/roam"
+      ;; org-roam-directory "~/txt/zattel"
       ;; org-roam-directory "~/txt/jethros-braindump"
       org-roam-dailies-directory "~/txt/roam/fleeting"
-      ;; org-roam-index-file "index.org"
+      org-roam-index-file "index.org"
       )
 
 (setq org-roam-capture-templates
