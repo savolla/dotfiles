47e50c10b7625dccb9a2dbaf5afb66f437c6d4ee
Oleksiy Nehlyadyuk <savolla@protonmail.com>
2021-12-11 12:12:52 +0300

    hard linked all the dotfiles

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
 # first install all the packages needed
-sudo pacman -S unclutter -y
-sudo pacman -S screenkey -y
-sudo pacman -S flameshot -y
-sudo pacman -S kitty -y
-sudo pacman -S brave-browser -y
-sudo pacman -S pavucontrol -y
-sudo pacman -S rofi -y
-sudo pacman -S emacs -y
-sudo pacman -S ranger -y
-sudo pacman -S sxiv -y
-sudo pacman -S signal-desktop -y
-sudo pacman -S retroarch -y
-sudo pacman -S bat -y
-sudo pacman -S pulseaudio-equalizer -y
-sudo pacman -S xcalib -y
-sudo pacman -S net-tools -y
-sudo pacman -S ttf-fira-code -y
-sudo pacman -S tmux -y
-sudo pacman -S htop -y
+sudo pacman -S \
+    unclutter \
+    screenkey \
+    flameshot \
+    kitty \
+    brave-browser \
+    pavucontrol \
+    rofi \
+    emacs \
+    ranger \
+    sxiv \
+    signal-desktop \
+    retroarch \
+    bat \
+    pulseaudio-equalizer \
+    xcalib \
+    net-tools \
+    ttf-fira-code \
+    tmux \
+    htop \
+    --needed
 
 # Place all contents inside .config
 for dotfile in .config/*; do
-    mv "$dotfile" "$USER"/.config
+    # cp -rva "$dotfile" ~/.config # hard copy
+    cp -lRf "$dotfile" ~/.config/
 done
 
 # place all single files in home
-mv ./.doom.d ~/
-mv ./.scripts ~/
-mv ./.bashrc ~/
-mv ./.dmenurc ~/
-mv ./.gdbinit ~/
-mv ./.profile ~/
-mv ./.tmux.conf ~/
-mv ./.tmux.conf ~/
+cp -lRf .doom.d ~/
+cp -lRf .scripts ~/
+cp -lRf .bashrc ~/
+cp -lRf .dmenurc ~/
+cp -lRf .gdbinit ~/
+cp -lRf .profile ~/
+cp -lRf .tmux.conf ~/
+cp -lRf .tmux.conf ~/
+
+echo "dotfiles are installed on the system"
