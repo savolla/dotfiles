a1e0013d7ec2f8e163bb3b977c7f1ef114539566
Oleksiy Nehlyadyuk <savolla@protonmail.com>
2021-12-12 21:24:45 +0300

    This is my code. My code is amazing.

--- .scripts/update-repos.sh
+++ .scripts/update-repos.sh
@@ -9,25 +9,25 @@
 COMMIT_MESSAGE=$(curl -s http://whatthecommit.com | perl -p0e '($_)=m{<p>(.+?)</p>}s')
 
 ## github projects:
-DOTFILES="$HOME/.git"
-NOTITIA="$HOME/txt"
-BRAINDUMP="$HOME/txt/roam"
+DOTFILES="$HOME/project/dev/dotfiles/.git"
+# NOTITIA="$HOME/txt"
+# BRAINDUMP="$HOME/txt/roam"
 
 ## dotfiles
-git --git-dir="$DOTFILES" --work-tree="$HOME" add -u
-git --git-dir="$DOTFILES" --work-tree="$HOME" commit -m "$COMMIT_MESSAGE"
-git --git-dir="$DOTFILES" --work-tree="$HOME" push -u origin main
+git --git-dir="$DOTFILES" add -u
+git --git-dir="$DOTFILES" commit -m "$COMMIT_MESSAGE"
+git --git-dir="$DOTFILES" push -u origin main
 
 ## notitia
-cd "$NOTITIA"
-git add -u
-git add images/
-git add notitia/
-git commit -m "$COMMIT_MESSAGE"
-git push -u origin main
+# cd "$NOTITIA"
+# git add -u
+# git add images/
+# git add notitia/
+# git commit -m "$COMMIT_MESSAGE"
+# git push -u origin main
 
-## braindump
-cd "$BRAINDUMP"
-git add *
-git commit -m "$COMMIT_MESSAGE"
-git push -u origin main
+# ## braindump
+# cd "$BRAINDUMP"
+# git add *
+# git commit -m "$COMMIT_MESSAGE"
+# git push -u origin main
