b3fffab003ccad6b59063d6348abd99b7b3bd679
savolla <savolla@protonmail.com>
2022-01-23 20:48:27 +0300

    totally more readable

--- .scripts/show-i3-keyboard-shortcuts.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-grep '^bindsym' /home/"$USER"/.config/i3/config | sed 's/$mod2/ALT/' | sed 's/$mod/WIN/' | sed 's/bindsym//' | sed 's/exec //' | sed 's/--no-startup-id //' | sed 's/#.*$//' | sed 's/--release //g' | sort | bat
