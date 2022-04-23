d3971230d2bd62851cd44c6294a14324138b35eb
savolla <savolla@protonmail.com>
2022-04-23 10:45:19 +0300

    QuickFix.

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -66,7 +66,7 @@
 (defun benmezger/org-roam-export-all ()
   "Re-exports all Org-roam files to Hugo markdown."
   (interactive)
-  (dolist (f (org-roam-node-find))
+  (dolist (f (org-roam--list-all-files))
     (with-current-buffer (find-file f)
       (when (s-contains? "SETUPFILE" (buffer-string))
         (org-hugo-export-wim-to-md)))))
