202df70956bf8f1e6b818f2a5594b9063630863f
savolla <savolla@protonmail.com>
2022-06-04 22:35:57 +0300

    ajax-loader hotness, oh yeah

--- .scripts/dyn-wlp.sh
+++ .scripts/dyn-wlp.sh
@@ -2,8 +2,11 @@
 
 CURRENT_HOUR=$(date "+%T" | awk -F ":" '{ print $1 }')
 HALF_OF_AN_HOUR=$((30*60))
+MIDNIGHT_WALLPAPER="$USER/.scripts/"
 while true
 do
-
-    sleep $HALF_OF_AN_HOUR
+    if [[ "CURRENT_HOUR" -gt "6" ]] && [[ "CURRENT_HOUR" -lt "11" ]]
+       feh --bg-fill
+        sleep $HALF_OF_AN_HOUR
+    fi
 done
