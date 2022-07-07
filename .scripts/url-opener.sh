393301a9e3f2371e534ad62c50558593bdcfbf0a
savolla <savolla@protonmail.com>
2022-07-07 20:37:53 +0000

    It's getting hard to keep up with the crap I've trashed

--- .scripts/url-opener.sh
+++ .scripts/url-opener.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 WEB_BROWSER="$(whereis firefox | cut -d ' ' -f 2)"
-SITES="archwiki\nrss\nwhoogle\nportainer\ngoogle\nheimdall\ntranslate\nprotonmail\ndiscord\nyoutube\ngitsearch\ngitprofile\nblog\nwikipedia\nhackernews\nlibgen\nzlib\nsearx\nstarpage\npiratebay\nuzem\ninvidious\nnews\nwrss\nwportainer\nwheimdall\nwgoogle\nwnextcloud\nwirc\n"
+SITES="bookstack\ngithub\nheadphones\nirc\ncloud\nmusic\ntodo\narchwiki\nrss\nwsearch\nportainer\nsearch\ndashboard\ntranslate\nprotonmail\ndiscord\nyoutube\ngitsearch\ngitprofile\nblog\nwikipedia\nhackernews\nlibgen\nzlib\nsearx\nstarpage\npiratebay\nuzem\ninvidious\nnews\nwrss\nwportainer\nwheimdall\nwsearch\nwcloud\nwirc\n"
 
 CHOICE=$(echo -e $SITES | rofi -dmenu -p "goto")
 
@@ -10,23 +10,37 @@ case $CHOICE in
     # savolla's portainer apps
     rss)
         $WEB_BROWSER "http://192.168.1.194:10000/i/" ;;
-    heimdall)
+    dashboard)
         $WEB_BROWSER "http://192.168.1.194" ;;
-    google)
+    search)
         $WEB_BROWSER "http://192.168.1.194:5000/" ;;
     portainer)
         $WEB_BROWSER "https://192.168.1.194:9000/#!/home" ;;
+    irc)
+        $WEB_BROWSER "http://192.168.1.194:64080" ;;
+    cloud)
+        $WEB_BROWSER "https://192.168.1.194:10101" ;;
+    todo)
+        $WEB_BROWSER "http://192.168.1.194:10016" ;;
+    music)
+        $WEB_BROWSER "http://192.168.1.194:10016" ;;
+    headphones)
+        $WEB_BROWSER "http://192.168.1.194:10016" ;;
+    github)
+        $WEB_BROWSER "http://192.168.1.194:3000" ;;
+    bookstack)
+        $WEB_BROWSER "http://192.168.1.194:6876" ;;
 
     # work shortcuts
     wrss)
         $WEB_BROWSER "http://192.168.68.194:10000/i/" ;;
-    wheimdall)
+    wdashboard)
         $WEB_BROWSER "http://192.168.68.194:81" ;;
-    wgoogle)
+    wsearch)
         $WEB_BROWSER "http://192.168.68.194:5000/" ;;
     wportainer)
         $WEB_BROWSER "http://192.168.68.194:9443/#!/home" ;;
-    wnextcloud)
+    wcloud)
         $WEB_BROWSER "https://192.168.68.194:10101/" ;;
     wirc)
         $WEB_BROWSER "http://192.168.68.194:64081/" ;;
